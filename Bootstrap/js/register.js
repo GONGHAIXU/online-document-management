7bb91c552c9fa8314100bc88483cb6498419ee4f
GONGHAIXU <3111398755@qq.com>
2019-07-11 15:09:50 +0800

    1

--- Bootstrap/js/register.js
+++ Bootstrap/js/register.js
@@ -32,6 +32,34 @@ function verifyByMail(){
         }
      });
 }
+function verifyByPhone(){
+	var username = $('#username').val();
+	var phone = $('#phone').val();
+	var password = $('#password').val();
+	var str={"username":username,
+			 "phone":phone,
+			 "password":password
+			 }
+	$.ajax({
+    type: "POST",
+	contentType: "application/json;charset=utf-8",
+    url: "http://localhost:8080/registerByPhone",
+    async:true,  
+    data: JSON.stringify(str),
+	dataType: "json",
+    success: function (data) {
+		if(data.result == "true"){
+			alert("验证码已发送，请耐心等待");
+		}
+		else{
+			alert(data.result);
+		}
+		},
+    error: function (error) {
+		alert("数据连接出错，请重试！");
+        }
+     });
+}
 function register(){
 	var activationCode = $('#activationCode').val();
 	var str={"activationCode":activationCode
